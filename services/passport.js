4b8ef6fc1ad1e5ff73f7661e87b9c543681d7f11
Brian Workman <brianworkman43055@gmail.com>
2019-01-01 20:34:31 -0500

    Oauth and mongo database setup

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,46 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose =  require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+    });
+});
 
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientId,
-        clientSecret: keys.googleSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-
-    })
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientId,
+            clientSecret: keys.googleSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if(existingUser) {
+                        // user found
+                        done(null, existingUser);
+                    } else {
+                        // new user, add it to the database
+                        new User({
+                                googleId: profile.id,
+                                firstName: profile.name.givenName,
+                                lastName: profile.name.familyName,
+                                image: profile._json.image.url
+                            })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                });
+        }
+    )
 );
