e058886fdea153db58908307a1613ddb97425454
Brian Workman <brian.workman43055@gmail.com>
2019-01-01 21:05:08 -0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientId: '926712794422-c097kga8arokkfm8aq2bde4hl40a2rkd.apps.googleusercontent.com',
-    googleSecret: '8VARvejC70cAwKVXSS2T4b0q',
-    mongoURI: 'mongodb://airbiscuit:Yzg89mLXTjbs@ds147344.mlab.com:47344/surveyme',
-    cookieKey: 'A6VVc5JwDevU1vZA3bE0k85nXN43jA3Q'
-};
-
