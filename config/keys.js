196f2d6a4712095984535456e314780dced0f6dc
Brian Workman <brianworkman43055@gmail.com>
2019-01-01 21:12:49 -0500

    Modified config keys to use env vars from heroku

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
-module.exports = {
-    googleClientId: '926712794422-c097kga8arokkfm8aq2bde4hl40a2rkd.apps.googleusercontent.com',
-    googleSecret: '8VARvejC70cAwKVXSS2T4b0q',
-    mongoURI: 'mongodb://airbiscuit:Yzg89mLXTjbs@ds147344.mlab.com:47344/surveyme',
-    cookieKey: 'A6VVc5JwDevU1vZA3bE0k85nXN43jA3Q'
-};
-
+// CCHECK HEROKU ENV VAR FOR ENV SETTING
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    googleSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONOGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
